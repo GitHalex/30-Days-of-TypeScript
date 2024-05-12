85c4c1b5ebc698d73b835dfb92b679b6c02139b3
GitHalex <calex1731@gmail.com>
2024-05-11 22:02:33 -0400

    dia one finished

--- Dia1/unoNivel.js
+++ Dia1/unoNivel.js
@@ -2,30 +2,21 @@
 // "Bienvenido a 30DaysOfTypeScript"
 /* los comentario puede hacer el codigo legible facil de usar
 e informativo */
-
 console.log("esto solo es una prueba en TS hola mundo");
-
-let nombre: string = "Halex";
-let numero: number = 2024;
-const married: boolean = false;
-let variableUndefined: undefined;
-let variableNull: null;
-
+var nombre = "Halex";
+var numero = 2024;
+var married = false;
+var variableUndefined;
+var variableNull;
 variableNull = null;
-
 console.log(typeof nombre);
 console.log(typeof numero);
 console.log(typeof married);
 console.log(typeof variableUndefined);
 console.log(typeof variableNull);
-
 // Declare cuatro variable sin asiganar valores
-let apellido: string = "hanyi",
-  trabajo: string = "software",
-  age: number = 12;
-
+var apellido = "hanyi", trabajo = "software", age = 12;
 console.log("Esto es una prueba de los tipos de datos");
-
 console.log(typeof apellido);
 console.log(typeof trabajo);
 console.log(typeof age);
