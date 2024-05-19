7dd85e1a6942df05ebb4fc7626d620c1f6eb975e
GitHalex <calex1731@gmail.com>
2024-05-18 22:05:35 -0400

    ejercicio de comparaciones y eh probado el operador ternario con else if y funciona

--- Dia4/unoNivel.js
+++ Dia4/unoNivel.js
@@ -1,10 +1,22 @@
-var edadInput = prompt("Ingrese su edad: ");
+/* let edadInput = prompt("Ingrese su edad: ");
 if (edadInput !== null) {
-    var edadUser = parseInt(edadInput);
-    edadUser >= 18
-        ? console.log("Tiene la edad suficiente para conducir")
-        : console.log("Te faltan ".concat(18 - edadUser, " a\u00F1os para conducir"));
+  let edadUser: number = parseInt(edadInput);
+  edadUser >= 18
+    ? console.log(`Tiene la edad suficiente para conducir`)
+    : console.log(`Te faltan ${18 - edadUser} años para conducir`);
+} else {
+  console.log("entrada cancelada por el usuario");
+} */
+var myAge = prompt("Ingrese su edad: ");
+var tuEdad = 30;
+if (myAge !== null) {
+    var miEdad = parseInt(myAge);
+    tuEdad > miEdad
+        ? console.log("Eres ".concat(tuEdad - miEdad, " mayor que yo"))
+        : miEdad > tuEdad
+            ? console.log("Yo soy ".concat(miEdad - tuEdad, " a\u00F1os mayor que tu"))
+            : console.log("tu edad: ".concat(tuEdad, " es igual a mi edad ").concat(miEdad));
 }
 else {
-    console.log("entrada cancelada por el usuario");
+    console.log("Entrada cancelada por el usuario");
 }
--- Dia4/unoNivel.ts
+++ Dia4/unoNivel.ts
@@ -1,4 +1,4 @@
-let edadInput = prompt("Ingrese su edad: ");
+/* let edadInput = prompt("Ingrese su edad: ");
 if (edadInput !== null) {
   let edadUser: number = parseInt(edadInput);
   edadUser >= 18
@@ -6,4 +6,17 @@ if (edadInput !== null) {
     : console.log(`Te faltan ${18 - edadUser} años para conducir`);
 } else {
   console.log("entrada cancelada por el usuario");
+} */
+
+let myAge = prompt("Ingrese su edad: ");
+let tuEdad: number = 30;
+if (myAge !== null) {
+  let miEdad: number = parseInt(myAge);
+  tuEdad > miEdad
+    ? console.log(`Eres ${tuEdad - miEdad} mayor que yo`)
+    : miEdad > tuEdad
+    ? console.log(`Yo soy ${miEdad - tuEdad} años mayor que tu`)
+    : console.log(`tu edad: ${tuEdad} es igual a mi edad ${miEdad}`);
+} else {
+  console.log("Entrada cancelada por el usuario");
 }
