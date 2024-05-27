841e340218ee7454a9e8a5e762e7912a16498d2e
GitHalex <calex1731@gmail.com>
2024-05-27 17:26:06 -0400

    ejericicio modifcado para mostrar los arreglos

---
+++ Dia5/dosNivel.js
@@ -0,0 +1,24 @@
+var countriesArr = [
+    "Albania",
+    "Bolivia",
+    "Canada",
+    "Denmark",
+    "Ethiopia",
+    "Finland",
+    "Germany",
+    "Hungary",
+    "Ireland",
+    "Japan",
+    "Kenya",
+];
+var webTechsArr = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Redux",
+    "Node",
+    "MongoDB",
+];
+console.log(countriesArr);
+console.log(webTechsArr);
---
+++ Dia5/dosNivel.ts
@@ -0,0 +1,26 @@
+const countriesArr: string[] = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+const webTechsArr: string[] = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+console.log(countriesArr);
+console.log(webTechsArr);
