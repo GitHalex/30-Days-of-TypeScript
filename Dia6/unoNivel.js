ed8c15cd3a3f718d3b60f2b8e315725b95bfec6a
GitHalex <calex1731@gmail.com>
2024-05-28 20:42:11 -0400

    Ejercicios 1 al 4 del nivel uno dia 6 terminado

---
+++ Dia6/unoNivel.js
@@ -0,0 +1,65 @@
+var paises = [
+    "Albania",
+    "Bolivia",
+    "Canada",
+    "Denmark",
+    "Ethiopia",
+    "Finland",
+    "Germany",
+    "Hungary",
+    "Ireland",
+    "Japan",
+    "Kenya",
+];
+var tecnologias = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Redux",
+    "Node",
+    "MongoDB",
+];
+var mernStack = ["MongoDB", "Express", "React", "Node"];
+/* for (let index = 0; index < 11; index++) {
+  console.log(index);
+} */
+/* let i: number = 0;
+while (i <= 10) {
+  console.log(i);
+
+  i = i + 1;
+} */
+/* let i: number = 0;
+do {
+  console.log(i);
+  i++;
+} while (i <= 10); */
+/* for (let index = 10; index > -1; index--) {
+  console.log(index);
+} */
+/* let i: number = 10;
+while (i > -1) {
+  console.log(i);
+
+  i--;
+} */
+/* let i: number = 10;
+do {
+  console.log(i);
+  i--;
+} while (i > -1); */
+/* let entradaInput = prompt("Ingrese un numero: ");
+if (entradaInput !== null) {
+  let entradaNumber: number = parseInt(entradaInput);
+  for (let i: number = 0; i <= entradaNumber; i++) {
+    console.log(i);
+  }
+} else {
+  console.log("Presionaste el prompt de cancelar");
+} */
+var cadenaAst = "";
+for (var i = 1; i < 8; i++) {
+    cadenaAst = "#" + cadenaAst;
+    console.log(cadenaAst);
+}
