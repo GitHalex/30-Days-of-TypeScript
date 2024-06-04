edcbdb3d084a06f78724402d68841f933a97692c
GitHalex <calex1731@gmail.com>
2024-06-03 22:41:18 -0400

    Ejercicio de funcion con dos paramtros

--- Dia7/unoNivel.js
+++ Dia7/unoNivel.js
@@ -6,3 +6,7 @@ function fullName(firstName, lastName) {
     return "Mi nombre es: ".concat(firstName, " y mi apellido es: ").concat(lastName);
 }
 console.log(fullName("Alex", "Lopez"));
+function addNumbers(numero1, numero2) {
+    return numero1 + numero2;
+}
+console.log(addNumbers(2, 4));
--- Dia7/unoNivel.ts
+++ Dia7/unoNivel.ts
@@ -7,3 +7,8 @@ function fullName(firstName: string, lastName: string): string {
   return `Mi nombre es: ${firstName} y mi apellido es: ${lastName}`;
 }
 console.log(fullName("Alex", "Lopez"));
+
+function addNumbers(numero1: number, numero2: number): number {
+  return numero1 + numero2;
+}
+console.log(addNumbers(2, 4));
