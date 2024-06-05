9fd9105bcb948063f366f4dc0cd7de5c52a2d9ed
GitHalex <calex1731@gmail.com>
2024-06-04 21:19:34 -0400

    Ejericicio de la velocidad terminado

--- Dia7/unoNivel.js
+++ Dia7/unoNivel.js
@@ -2,32 +2,52 @@
   console.log(`Mi nombre completo es: Alex Lopez`);
 }
 fullName(); */
-function fullName(firstName, lastName) {
-    return "Mi nombre es: ".concat(firstName, " y mi apellido es: ").concat(lastName);
+/* function fullName(firstName: string, lastName: string): string {
+  return `Mi nombre es: ${firstName} y mi apellido es: ${lastName}`;
 }
-console.log(fullName("Alex", "Lopez"));
-function addNumbers(numero1, numero2) {
-    return numero1 + numero2;
+console.log(fullName("Alex", "Lopez")); */
+/* function addNumbers(numero1: number, numero2: number): number {
+  return numero1 + numero2;
 }
-console.log(addNumbers(2, 4));
-function areaOfRectangle(ancho, alto) {
-    var area = ancho * alto;
-    return "El area es: ".concat(area);
+console.log(addNumbers(2, 4)); */
+/* function areaOfRectangle(ancho: number, alto: number): string {
+  let area: number = ancho * alto;
+  return `El area es: ${area}`;
 }
-console.log(areaOfRectangle(2, 4));
-function perimeterOfRectangle(length, width) {
-    var perimeter = 2 * (length + width);
-    return "el perimetro del rectangulo es:  ".concat(perimeter);
+console.log(areaOfRectangle(2, 4)); */
+/* function perimeterOfRectangle(length: number, width: number): string {
+  let perimeter: number = 2 * (length + width);
+  return `el perimetro del rectangulo es:  ${perimeter}`;
 }
-console.log(perimeterOfRectangle(2, 2));
-function volumeOfRectPrism(len, wid, hei) {
-    var volumen = len * wid * hei;
-    return "El volumen de un prisma rectangular es: ".concat(volumen, ": ");
+console.log(perimeterOfRectangle(2, 2)); */
+/* function volumeOfRectPrism(len: number, wid: number, hei: number): string {
+  let volumen: number = len * wid * hei;
+  return `El volumen de un prisma rectangular es: ${volumen}: `;
 }
-console.log(volumeOfRectPrism(2, 2, 2));
-var PI = Math.PI;
-function areaOfCircle(radio) {
-    var areaCirculo = PI * Math.pow(radio, 2);
-    return "El area del circulo es: ".concat(Math.floor(areaCirculo));
+console.log(volumeOfRectPrism(2, 2, 2)); */
+/* const PI: number = Math.PI;
+function areaOfCircle(radio: number): string {
+  let areaCirculo: number = PI * radio ** 2;
+  return `El area del circulo es: ${Math.floor(areaCirculo)}`;
 }
-console.log(areaOfCircle(2));
+console.log(areaOfCircle(2)); */
+/* const PI: number = Math.PI;
+function circumOfCircle(radio: number): number {
+  return PI * radio * 2;
+}
+console.log(`La circunferencia de un circulo es: ${circumOfCircle(2)}`); */
+function calculateSpeed(distance, time) {
+    if (time === 0) {
+        throw new Error("El tiempo no puede ser cero.");
+    }
+    return distance / time;
+}
+var distanceTravelled = 100; // en metros
+var timeTaken = 10; // en segundos
+try {
+    var speed = calculateSpeed(distanceTravelled, timeTaken);
+    console.log("La velocidad del objeto en movimiento es ".concat(speed, " m/s"));
+}
+catch (error) {
+    console.error(error.message);
+}
--- Dia7/unoNivel.ts
+++ Dia7/unoNivel.ts
@@ -3,41 +3,60 @@
 }
 fullName(); */
 
-function fullName(firstName: string, lastName: string): string {
+/* function fullName(firstName: string, lastName: string): string {
   return `Mi nombre es: ${firstName} y mi apellido es: ${lastName}`;
 }
-console.log(fullName("Alex", "Lopez"));
+console.log(fullName("Alex", "Lopez")); */
 
-function addNumbers(numero1: number, numero2: number): number {
+/* function addNumbers(numero1: number, numero2: number): number {
   return numero1 + numero2;
 }
-console.log(addNumbers(2, 4));
+console.log(addNumbers(2, 4)); */
 
-function areaOfRectangle(ancho: number, alto: number): string {
+/* function areaOfRectangle(ancho: number, alto: number): string {
   let area: number = ancho * alto;
   return `El area es: ${area}`;
 }
+console.log(areaOfRectangle(2, 4)); */
 
-console.log(areaOfRectangle(2, 4));
-
-function perimeterOfRectangle(length: number, width: number): string {
+/* function perimeterOfRectangle(length: number, width: number): string {
   let perimeter: number = 2 * (length + width);
   return `el perimetro del rectangulo es:  ${perimeter}`;
 }
+console.log(perimeterOfRectangle(2, 2)); */
 
-console.log(perimeterOfRectangle(2, 2));
-
-function volumeOfRectPrism(len: number, wid: number, hei: number): string {
+/* function volumeOfRectPrism(len: number, wid: number, hei: number): string {
   let volumen: number = len * wid * hei;
   return `El volumen de un prisma rectangular es: ${volumen}: `;
 }
+console.log(volumeOfRectPrism(2, 2, 2)); */
 
-console.log(volumeOfRectPrism(2, 2, 2));
-
-const PI: number = Math.PI;
+/* const PI: number = Math.PI;
 function areaOfCircle(radio: number): string {
   let areaCirculo: number = PI * radio ** 2;
   return `El area del circulo es: ${Math.floor(areaCirculo)}`;
 }
+console.log(areaOfCircle(2)); */
 
-console.log(areaOfCircle(2));
+/* const PI: number = Math.PI;
+function circumOfCircle(radio: number): number {
+  return PI * radio * 2;
+}
+console.log(`La circunferencia de un circulo es: ${circumOfCircle(2)}`); */
+
+function calculateSpeed(distance: number, time: number): number {
+  if (time === 0) {
+    throw new Error("El tiempo no puede ser cero.");
+  }
+  return distance / time;
+}
+
+const distanceTravelled: number = 100; // en metros
+const timeTaken: number = 10; // en segundos
+
+try {
+  const speed: number = calculateSpeed(distanceTravelled, timeTaken);
+  console.log(`La velocidad del objeto en movimiento es ${speed} m/s`);
+} catch (error) {
+  console.error(error.message);
+}
