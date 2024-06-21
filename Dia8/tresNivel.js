73ee5ad4774329f12852fb235e8330f2d968a72a
GitHalex <calex1731@gmail.com>
2024-06-20 23:20:55 -0400

    ejejrart

--- Dia8/tresNivel.js
+++ Dia8/tresNivel.js
@@ -1,39 +1,107 @@
-var personAccount = {
-    firstName: "John",
-    lastName: "Doe",
-    incomes: [
-        { description: "Salary", amount: 3000 },
-        { description: "Freelance", amount: 1500 },
-    ],
-    expenses: [
-        { description: "Rent", amount: 1000 },
-        { description: "Groceries", amount: 300 },
-    ],
-    totalIncome: function () {
-        return this.incomes.reduce(function (acc, income) { return acc + income.amount; }, 0);
-    },
-    totalExpense: function () {
-        return this.expenses.reduce(function (acc, expense) { return acc + expense.amount; }, 0);
-    },
-    accountInfo: function () {
-        return "Account Holder: ".concat(this.firstName, " ").concat(this.lastName);
-    },
-    addIncome: function (description, amount) {
-        this.incomes.push({ description: description, amount: amount });
-    },
-    addExpense: function (description, amount) {
-        this.expenses.push({ description: description, amount: amount });
-    },
-    accountBalance: function () {
-        return this.totalIncome() - this.totalExpense();
-    },
+/* interface Income {
+  description: string;
+  amount: number;
+}
+
+interface Expense {
+  description: string;
+  amount: number;
+}
+
+interface PersonAccount {
+  firstName: string;
+  lastName: string;
+  incomes: Income[];
+  expenses: Expense[];
+  totalIncome: () => number;
+  totalExpense: () => number;
+  accountInfo: () => string;
+  addIncome: (description: string, amount: number) => void;
+  addExpense: (description: string, amount: number) => void;
+  accountBalance: () => number;
+}
+
+const personAccount: PersonAccount = {
+  firstName: "John",
+  lastName: "Doe",
+  incomes: [
+    { description: "Salary", amount: 3000 },
+    { description: "Freelance", amount: 1500 },
+  ],
+  expenses: [
+    { description: "Rent", amount: 1000 },
+    { description: "Groceries", amount: 300 },
+  ],
+  totalIncome() {
+    return this.incomes.reduce((acc, income) => acc + income.amount, 0);
+  },
+  totalExpense() {
+    return this.expenses.reduce((acc, expense) => acc + expense.amount, 0);
+  },
+  accountInfo() {
+    return `Account Holder: ${this.firstName} ${this.lastName}`;
+  },
+  addIncome(description, amount) {
+    this.incomes.push({ description, amount });
+  },
+  addExpense(description, amount) {
+    this.expenses.push({ description, amount });
+  },
+  accountBalance() {
+    return this.totalIncome() - this.totalExpense();
+  },
 };
+
 console.log(personAccount.accountInfo()); // Account Holder: John Doe
 console.log(personAccount.totalIncome()); // 4500
 console.log(personAccount.totalExpense()); // 1300
 console.log(personAccount.accountBalance()); // 3200
+
 personAccount.addIncome("Stock Dividends", 200);
 console.log(personAccount.totalIncome()); // 4700
+
 personAccount.addExpense("Utilities", 150);
 console.log(personAccount.totalExpense()); // 1450
-console.log(personAccount.accountBalance()); // 3250
+console.log(personAccount.accountBalance()); // 3250 */
+var usersTres = [
+    {
+        _id: "ab12ex",
+        username: "Alex",
+        email: "alex@alex.com",
+        password: "123123",
+        createdAt: "08/01/2020 9:00 AM",
+        isLoggedIn: false,
+    },
+    {
+        _id: "fg12cy",
+        username: "Asab",
+        email: "asab@asab.com",
+        password: "123456",
+        createdAt: "08/01/2020 9:30 AM",
+        isLoggedIn: true,
+    },
+    {
+        _id: "zwf8md",
+        username: "Brook",
+        email: "brook@brook.com",
+        password: "123111",
+        createdAt: "08/01/2020 9:45 AM",
+        isLoggedIn: true,
+    },
+    {
+        _id: "eefamr",
+        username: "Martha",
+        email: "martha@martha.com",
+        password: "123222",
+        createdAt: "08/01/2020 9:50 AM",
+        isLoggedIn: false,
+    },
+    {
+        _id: "ghderc",
+        username: "Thomas",
+        email: "thomas@thomas.com",
+        password: "123333",
+        createdAt: "08/01/2020 10:00 AM",
+        isLoggedIn: false,
+    },
+];
--- Dia8/tresNivel.ts
+++ Dia8/tresNivel.ts
@@ -1,4 +1,4 @@
-interface Income {
+/* interface Income {
   description: string;
   amount: number;
 }
@@ -62,4 +62,56 @@ console.log(personAccount.totalIncome()); // 4700
 
 personAccount.addExpense("Utilities", 150);
 console.log(personAccount.totalExpense()); // 1450
-console.log(personAccount.accountBalance()); // 3250
+console.log(personAccount.accountBalance()); // 3250 */
+
+interface User {
+  _id: string;
+  username: string;
+  email: string;
+  password: string;
+  createdAt: string;
+  isLoggedIn: boolean;
+}
+
+const usersTres: User[] = [
+  {
+    _id: "ab12ex",
+    username: "Alex",
+    email: "alex@alex.com",
+    password: "123123",
+    createdAt: "08/01/2020 9:00 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "fg12cy",
+    username: "Asab",
+    email: "asab@asab.com",
+    password: "123456",
+    createdAt: "08/01/2020 9:30 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "zwf8md",
+    username: "Brook",
+    email: "brook@brook.com",
+    password: "123111",
+    createdAt: "08/01/2020 9:45 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "eefamr",
+    username: "Martha",
+    email: "martha@martha.com",
+    password: "123222",
+    createdAt: "08/01/2020 9:50 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "ghderc",
+    username: "Thomas",
+    email: "thomas@thomas.com",
+    password: "123333",
+    createdAt: "08/01/2020 10:00 AM",
+    isLoggedIn: false,
+  },
+];
