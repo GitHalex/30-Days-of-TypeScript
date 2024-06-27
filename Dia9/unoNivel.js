25cbb894a42b07fad5a8389993515adfea1f297f
GitHalex <calex1731@gmail.com>
2024-06-27 11:26:12 -0400

    ejercicio donde definimos una funcion callback con una cadena

--- Dia9/unoNivel.js
+++ Dia9/unoNivel.js
@@ -34,17 +34,28 @@ function cube(callback, n) {
     return callback(n) * n;
 }
 console.log(cube(callback, 3));
-var sumArray = function (arr) {
-    var suma = 0;
-    arr.forEach(function (element) {
-        suma += element;
-    });
-    return suma;
+var callbackString = function (cadena) {
+    return cadena.toLowerCase();
 };
-console.log(sumArray(numbers));
+function lowerCase(callbackString, cadena) {
+    return callbackString(cadena) + " Esta en minuscula";
+}
+console.log(lowerCase(callbackString, "AlEx"));
+/* const sumArray = (arr: number[]): number => {
+  let suma: number = 0;
+
+  arr.forEach(function (element: number) {
+    suma += element;
+  });
+
+  return suma;
+};
+
+console.log(sumArray(numbers)); */
 countriesArreglo.forEach(function (element, i) {
     console.log("".concat(i, " -> ").concat(element.toUpperCase()));
 });
+console.log(countriesArreglo);
 var mayusculasPaises = countriesArreglo.map(function (pais) { return pais.toUpperCase(); });
 console.log(mayusculasPaises);
 // Definir el tipo del array de entrada
@@ -52,3 +63,53 @@ var numbersArr = [1, 2, 3, 4, 5];
 // Usar la función map con una función flecha tipada
 var numbersSquare = numbersArr.map(function (num) { return num * num; });
 console.log(numbersSquare);
+var countriesContaingLand = countriesArreglo.filter(function (pais) { return pais.length === 7; });
+console.log(countriesContaingLand);
+var scores = [
+    { name: "Asabeneh", score: 95 },
+    { name: "Lidiya", score: 98 },
+    { name: "Mathias", score: 80 },
+    { name: "Elias", score: 50 },
+    { name: "Martha", score: 85 },
+    { name: "John", score: 100 },
+];
+var scoresGreaterEighty = scores.filter(function (score) { return score.score > 80; });
+console.log(scoresGreaterEighty);
+var sum = numbers.reduce(function (acc, cur) { return acc + cur; }, 0);
+console.log(sum); // Salida: 15
+var fruits = [
+    { name: "apple", quantity: 5 },
+    { name: "banana", quantity: 3 },
+    { name: "orange", quantity: 4 },
+];
+var totalFruits = fruits.reduce(function (acc, fruit) { return acc + fruit.quantity; }, 0);
+console.log(totalFruits); // Salida: 12
+var areAllStr = countriesArreglo.every(function (pais) { return typeof pais === "string"; });
+console.log("la lista: ".concat(countriesArreglo, " son todos de tipo cadena => ").concat(areAllStr));
+var producto = products.find(function (product) { return product.product.length > 4; });
+if (producto) {
+    console.log(producto);
+}
+else {
+    console.log("No se encontró ningún producto con un nombre de más de 4 caracteres.");
+}
+var result = numbers.findIndex(function (name) { return name >= 7; });
+console.log(result);
+var someAllStr = countriesArreglo.some(function (pais) { return typeof pais === "string"; });
+console.log("la lista: ".concat(countriesArreglo, " son algunos de tipo cadena => ").concat(someAllStr));
+var numbersFloat = [9.81, 3.14, 100, 37];
+console.log(numbers.sort()); // Ordenación incorrecta: [100, 3.14, 37, 9.81]
+numbers.sort(function (a, b) { return a - b; });
+console.log(numbers); // Ordenación correcta: [3.14, 9.81, 37, 100]
+numbers.sort(function (a, b) { return b - a; });
+console.log(numbers); // Ordenación correcta: [100, 37, 9.81, 3.14]
+/* const users: User[] = [
+  { name: "Asabeneh", age: 150 },
+  { name: "Brook", age: 50 },
+  { name: "Eyob", age: 100 },
+  { name: "Elias", age: 22 },
+];
+
+users.sort((a: User, b: User): number => a.age - b.age);
+
+console.log(users); */
--- Dia9/unoNivel.ts
+++ Dia9/unoNivel.ts
@@ -35,11 +35,11 @@ clearInterval(intervalId);
 // Usar setTimeout y guardar el identificador del temporizador con el tipo number
 const timeoutId: number = setTimeout(sayHello, 2000); // Imprime "Hello" después de 2 segundos
 // Si necesitas limpiar el temporizador antes de que se ejecute, usa clearTimeout
+
 // clearTimeout(timeoutId);
 
 // Definimos el tipo de la función callback
 type CallbackFunction = (n: number) => number;
-
 // Una función callback, el nombre de la función puede ser cualquier nombre
 const callback: CallbackFunction = (n: number): number => {
   return n ** 2;
@@ -52,7 +52,21 @@ function cube(callback: CallbackFunction, n: number): number {
 
 console.log(cube(callback, 3));
 
-const sumArray = (arr: number[]): number => {
+type CallbackFunctionString = (cadena: string) => string;
+
+const callbackString: CallbackFunctionString = (cadena: string): string => {
+  return cadena.toLowerCase();
+};
+
+function lowerCase(
+  callbackString: CallbackFunctionString,
+  cadena: string
+): string {
+  return callbackString(cadena) + " Esta en minuscula";
+}
+console.log(lowerCase(callbackString, "AlEx"));
+
+/* const sumArray = (arr: number[]): number => {
   let suma: number = 0;
 
   arr.forEach(function (element: number) {
@@ -62,11 +76,12 @@ const sumArray = (arr: number[]): number => {
   return suma;
 };
 
-console.log(sumArray(numbers));
+console.log(sumArray(numbers)); */
 
 countriesArreglo.forEach((element: string, i: number): void => {
   console.log(`${i} -> ${element.toUpperCase()}`);
 });
+console.log(countriesArreglo);
 
 const mayusculasPaises: string[] = countriesArreglo.map(
   (pais: string): string => pais.toUpperCase()
@@ -82,3 +97,98 @@ const numbersSquare: number[] = numbersArr.map(
 );
 
 console.log(numbersSquare);
+
+const countriesContaingLand: string[] = countriesArreglo.filter(
+  (pais: string) => pais.length === 7
+);
+console.log(countriesContaingLand);
+
+interface Score {
+  name: string;
+  score: number;
+}
+const scores: Score[] = [
+  { name: "Asabeneh", score: 95 },
+  { name: "Lidiya", score: 98 },
+  { name: "Mathias", score: 80 },
+  { name: "Elias", score: 50 },
+  { name: "Martha", score: 85 },
+  { name: "John", score: 100 },
+];
+
+const scoresGreaterEighty = scores.filter((score) => score.score > 80);
+console.log(scoresGreaterEighty);
+
+const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+
+console.log(sum); // Salida: 15
+
+interface Fruit {
+  name: string;
+  quantity: number;
+}
+
+const fruits: Fruit[] = [
+  { name: "apple", quantity: 5 },
+  { name: "banana", quantity: 3 },
+  { name: "orange", quantity: 4 },
+];
+
+const totalFruits = fruits.reduce((acc, fruit) => acc + fruit.quantity, 0);
+
+console.log(totalFruits); // Salida: 12
+
+const areAllStr: boolean = countriesArreglo.every(
+  (pais) => typeof pais === "string"
+);
+console.log(
+  `la lista: ${countriesArreglo} son todos de tipo cadena => ${areAllStr}`
+);
+
+const producto: Product | undefined = products.find(
+  (product) => product.product.length > 4
+);
+
+if (producto) {
+  console.log(producto);
+} else {
+  console.log(
+    "No se encontró ningún producto con un nombre de más de 4 caracteres."
+  );
+}
+
+const result: number = numbers.findIndex((name) => name >= 7);
+console.log(result);
+
+const someAllStr: boolean = countriesArreglo.some(
+  (pais) => typeof pais === "string"
+);
+console.log(
+  `la lista: ${countriesArreglo} son algunos de tipo cadena => ${someAllStr}`
+);
+
+const numbersFloat: number[] = [9.81, 3.14, 100, 37];
+
+console.log(numbers.sort()); // Ordenación incorrecta: [100, 3.14, 37, 9.81]
+
+numbers.sort((a: number, b: number): number => a - b);
+console.log(numbers); // Ordenación correcta: [3.14, 9.81, 37, 100]
+
+numbers.sort((a: number, b: number): number => b - a);
+console.log(numbers); // Ordenación correcta: [100, 37, 9.81, 3.14]
+
+interface User {
+  name: string;
+  age: number;
+}
+
+/* const users: User[] = [
+  { name: "Asabeneh", age: 150 },
+  { name: "Brook", age: 50 },
+  { name: "Eyob", age: 100 },
+  { name: "Elias", age: 22 },
+];
+
+users.sort((a: User, b: User): number => a.age - b.age);
+
+console.log(users); */
