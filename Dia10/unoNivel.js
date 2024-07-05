ff5efb5b25fc1a584e1fcd5e90c80c4e83252bf5
GitHalex <calex1731@gmail.com>
2024-07-04 21:36:48 -0400

    Ejercicios de set puestos nada mas es2026 no lo soporta mas

--- Dia10/unoNivel.js
+++ Dia10/unoNivel.js
@@ -1,11 +1,42 @@
-console.log("entonces");
-var names = ["Asabeneh", "Brook", "David", "John"];
-var firstPerson = names[0], secondPerson = names[1], thirdPerson = names[2], fourthPerson = names[3];
-console.log(firstPerson, secondPerson, thirdPerson, fourthPerson);
-var fullStack = [
-    ["HTML", "CSS", "JS", "React"],
-    ["Node", "Express", "MongoDB"],
+var mySet = new Set();
+console.log(typeof mySet);
+mySet.add(1);
+console.log(mySet);
+var stringSet = new Set();
+/* interface Person {
+  name: string;
+  age: number;
+}
+
+const personSet: Set<Person> = new Set<Person>(); */
+/* const languages: string[] = [
+  "English",
+  "Finnish",
+  "English",
+  "French",
+  "Spanish",
+  "English",
+  "French",
 ];
-var frontEndARR = fullStack[0], backEndARR = fullStack[1];
+
+// Crear un conjunto a partir del array de lenguajes
+const setOfLanguages: Set<string> = new Set<string>(languages);
+
+// Imprimir el conjunto en la consola
+console.log(setOfLanguages); */
+/* console.log("entonces");
+const names: string[] = ["Asabeneh", "Brook", "David", "John"];
+const [firstPerson, secondPerson, thirdPerson, fourthPerson] = names;
+
+console.log(firstPerson, secondPerson, thirdPerson, fourthPerson);
+
+const fullStack: [string[], string[]] = [
+  ["HTML", "CSS", "JS", "React"],
+  ["Node", "Express", "MongoDB"],
+];
+
+const [frontEndARR, backEndARR]: [string[], string[]] = fullStack;
+
 console.log(frontEndARR);
 console.log(backEndARR);
+ */
--- Dia10/unoNivel.ts
+++ Dia10/unoNivel.ts
@@ -1,4 +1,34 @@
-console.log("entonces");
+const mySet: Set<number> = new Set<number>();
+console.log(typeof mySet);
+mySet.add(1);
+console.log(mySet);
+
+const stringSet: Set<string> = new Set<string>();
+
+/* interface Person {
+  name: string;
+  age: number;
+}
+
+const personSet: Set<Person> = new Set<Person>(); */
+
+/* const languages: string[] = [
+  "English",
+  "Finnish",
+  "English",
+  "French",
+  "Spanish",
+  "English",
+  "French",
+];
+
+// Crear un conjunto a partir del array de lenguajes
+const setOfLanguages: Set<string> = new Set<string>(languages);
+
+// Imprimir el conjunto en la consola
+console.log(setOfLanguages); */
+
+/* console.log("entonces");
 const names: string[] = ["Asabeneh", "Brook", "David", "John"];
 const [firstPerson, secondPerson, thirdPerson, fourthPerson] = names;
 
@@ -13,3 +43,4 @@ const [frontEndARR, backEndARR]: [string[], string[]] = fullStack;
 
 console.log(frontEndARR);
 console.log(backEndARR);
+ */
