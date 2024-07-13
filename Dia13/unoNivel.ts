6b743e0556ee32f7248550e29649d1fcb2424d5b
GitHalex <calex1731@gmail.com>
2024-07-12 23:59:21 -0400

    Ejercicio de mostrar tablas con las consolas

---
+++ Dia13/unoNivel.ts
@@ -0,0 +1,39 @@
+console.log("%c30 Days Of JavaScript", "color:green"); // la salida del registro es verde
+console.log(
+  "%c30 Days%c %cOf%c %cJavaScript%c",
+  "color:green",
+  "",
+  "color:red",
+  "",
+  "color:yellow"
+); // salida del registro texto verde rojo y amarillo
+
+console.warn("This is a warning");
+console.warn(
+  "You are using React. Do not touch the DOM. Virtual DOM will take care of handling the DOM!"
+);
+console.warn("Warning is different from error");
+
+console.error("This is an error message");
+console.error("We all make mistakes");
+
+const namesTabla: string[] = ["Asabeneh", "Brook", "David", "John"];
+console.table(namesTabla);
+
+interface UserTable {
+  name: string;
+  title: string;
+  country: string;
+  city: string;
+  age: number;
+}
+
+const userTabla: UserTable = {
+  name: "Alex",
+  title: "Programmer",
+  country: "Bolivia",
+  city: "Bolivia",
+  age: 30,
+};
+
+console.table(userTabla);
